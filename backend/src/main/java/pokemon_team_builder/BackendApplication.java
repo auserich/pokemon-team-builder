1b0ef7790b66b5ee7a841eb3c05ab2a54a17afbc
Austin <auserich@gmail.com>
2024-01-15 09:53:37 -0800

    Initial commit

---
+++ backend/src/main/java/pokemon_team_builder/BackendApplication.java
@@ -0,0 +1,13 @@
+package pokemon_team_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pokemon_team_builder/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pokemon_team_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
